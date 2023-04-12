0969410878a30f76ca3e1c39cd9964e34a7ed7e1
Polaris9487 <109193430+Polaris9487@users.noreply.github.com>
2023-04-12 16:35:14 +0800

    Add files via upload

---
+++ aws-live-main/config.py
@@ -0,0 +1,6 @@
+customhost = "database-1.covmrntvvl5a.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "leejiapoh-employee"
+customregion = "us-east-1"
